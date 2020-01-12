24f07884bd8723f9cf0f01941fbc9458f8110f4d
unknown <pchyo92@gmail.com>
2020-01-12 14:31:45 +0900

    add all files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import VideoList from "./components/VideoList";
+import Footer from "./components/Footer";
+import Join from "./components/Join";
+import Login from "./components/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "@fortawesome/fontawesome-free/css/all.css";
+import axios from "axios";
+
+class App extends Component {
+  state = {
+    data: []
+  };
+
+  async componentDidMount() {
+    const { data } = await axios.get("http://localhost:4000/api/video/all");
+    const {
+      result: { videos }
+    } = data;
+    this.setState({ data: videos });
+    console.log(this.state.data);
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
+      <React.Fragment>
+        <Header />
+
+        <div className="content">
+          <Switch>
+            <Route path="/join" component={Join} />
+            <Route path="/login" component={Login} />
+            <Route path="/" render={props => <VideoList videos={data} />} />
+          </Switch>
+        </div>
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
